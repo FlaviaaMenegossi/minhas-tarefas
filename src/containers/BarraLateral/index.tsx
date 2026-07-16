b25d20ecd4474f78bffc17406d330ac9b7666520
FlaviaaMenegossi <flavinha.mene12@outlook.com>
2026-07-16 14:51:33 -0300

    parametrizando filtros

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -6,12 +6,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard ativo contador={1} legenda="pendentes" />
+        <FiltroCard contador={2} legenda="concluídas" />
+        <FiltroCard contador={3} legenda="urgentes" />
+        <FiltroCard contador={4} legenda="importantes" />
+        <FiltroCard contador={5} legenda="normal" />
+        <FiltroCard contador={10} legenda="todas" />
       </S.Filtros>
     </div>
   </S.Aside>
