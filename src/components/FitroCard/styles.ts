649d29cb5a50449a3d0d6e30fd1fd12ab824268f
FlaviaaMenegossi <flavinha.mene12@outlook.com>
2026-07-16 13:27:37 -0300

    construindo componente de filtro

--- src/components/FitroCard/styles.ts
+++ src/components/FitroCard/styles.ts
@@ -1,14 +1,20 @@
+// styles.ts
 import styled from 'styled-components'
 
-export const Card = styled.div`
+type CardProps = {
+  $ativo?: boolean
+}
+
+export const Card = styled.div<CardProps>`
   padding: 8px;
-  border: 1px solid #a1a1a1;
-  background-color: #fcfcfc;
-  color: #5e5e5e;
+  border: 1px solid ${(props) => (props.$ativo ? '#1E90FF' : '#a1a1a1')};
+  background-color: ${(props) => (props.$ativo ? '#fff' : '#fcfcfc')};
+  color: ${(props) => (props.$ativo ? '#1E90FF' : '#5e5e5e')};
+  border-radius: 8px;
 `
 
 export const Contador = styled.span`
-  color: #5e5e5e;
+  color: inherit;
   font-weight: bold;
   font-size: 24px;
   display: block;
