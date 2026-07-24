2d886cd3411123018098d122dc130cbb1d42a875
FlaviaaMenegossi <flavinha.mene12@outlook.com>
2026-07-24 13:22:24 -0300

    filtrando tarefas finalização

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -21,35 +21,29 @@ const BarraLateral = () => {
           <FiltroCard
             valor={enums.Status.PENDENTE}
             criterio="status"
-            ativo
-            contador={1}
             legenda="pendentes"
           />
           <FiltroCard
             valor={enums.Status.CONCLUIDA}
             criterio="status"
-            contador={2}
             legenda="concluídas"
           />
           <FiltroCard
             valor={enums.Prioridade.URGENTE}
             criterio="prioridade"
-            contador={3}
             legenda="urgentes"
           />
           <FiltroCard
             valor={enums.Prioridade.IMPORTANTE}
             criterio="prioridade"
-            contador={4}
             legenda="importantes"
           />
           <FiltroCard
             valor={enums.Prioridade.NORMAL}
             criterio="prioridade"
-            contador={5}
             legenda="normal"
           />
-          <FiltroCard criterio="todas" contador={10} legenda="todas" />
+          <FiltroCard criterio="todas" legenda="todas" />
         </S.Filtros>
       </div>
     </S.Aside>
